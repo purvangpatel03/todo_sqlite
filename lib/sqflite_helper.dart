40ee21d544ae049859d819db3c785db2d331d638
Purvang <purvangsuvagiya@gmail.com>
2024-05-01 15:07:55 +0530

    todo_sqflite

--- lib/sqflite_helper.dart
+++ lib/sqflite_helper.dart
@@ -2,42 +2,70 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class SqfliteHelper {
-
+  // all methods in this class are asynchronous because they involve I/O operations.
   Future<Database> openMyDatabase() async {
     return await openDatabase(
-      join(await getDatabasesPath(), 'myToDoDatabase.db'),
-      version: 1,
-      onCreate: (db, version) {
-        return db.execute(
-          'CREATE TABLE todoList(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title TEXT, status  INTEGER)',
-        );
-      },
-    );
+        // join method is used to join the path of the database with the path of the app's document directory.
+        join(await getDatabasesPath(), 'myToDoDatabase.db'),
+        // The version of the database. This is used to manage database schema changes.
+        version: 1,
+        // onCreate is a callback function that is called ONLY when the database is created for the first time.
+        onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE todoList(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title TEXT, status  INTEGER)',
+      );
+      //Here we are creating a table named todoList with three columns: id, title, and status.
+      //The id column is the primary key and is set to autoincrement.
+      //We use INTEGER for the status column because SQLite does not have a boolean data type.
+      //Instead, we use 0 for false and 1 for true.
+    });
   }
 
-  Future<void> insertTask(String title, bool status) async{
+  Future<void> insertTask(String title, bool status) async {
+    //db is the instance of the database that we get from the openMyDatabase function.
     final db = await openMyDatabase();
-    db.insert('todoList', {
-      'title': title,
-      'status': status ? 1 : 0,
-    }, conflictAlgorithm: ConflictAlgorithm.replace);
+    //after getting the database instance, we insert the task into the todoList table.
+    //insert method takes three arguments: the name of the table, the data to be inserted, and the conflictAlgorithm.
+    //data is a map with the column names as keys and the values to be inserted as values.
+    //We use ConflictAlgorithm.replace to replace the task if it already exists.
+    //here we don't need to insert the id column because it is set to autoincrement.
+    db.insert(
+        'todoList',
+        {
+          'title': title,
+          'status': status ? 1 : 0,
+          //We use 1 for true and 0 for false.
+        },
+        conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
-  Future<void> deleteTask(int id) async{
+  Future<void> deleteTask(int id) async {
     final db = await openMyDatabase();
+    //delete method takes two arguments: the name of the table and the where clause.
+    //we are using unique id for each task as the where clause to delete the task with the given id.
     db.delete('todoList', where: 'id = ?', whereArgs: [id]);
   }
 
-  Future<void> updateTask(int id, bool status) async{
+  Future<void> updateTask(int id, bool status) async {
     final db = await openMyDatabase();
-    db.update('todoList', {
-      'status': status ? 1 : 0,
-    }, where: 'id = ?', whereArgs: [id]);
+    //update method takes four arguments: the name of the table, the data to be updated, the where clause, and the whereArgs.
+    //In this case, we are updating the status of the task with the given id.
+    db.update(
+        'todoList',
+        {
+          'status': status ? 1 : 0,
+          //We use 1 for true and 0 for false.
+        },
+        where: 'id = ?',
+        whereArgs: [id]);
   }
 
-  Future<List<Map<String, dynamic>>> getTasks() async{
+  Future<List<Map<String, dynamic>>> getTasks() async {
     final db = await openMyDatabase();
+    //query method is used to get the tasks from the todoList table.
+    //It takes the name of the table as an argument.
+    //and returns a list of maps where each map represents a task.
+    //like [{id: 1, title: 'Task 1', status: 1}, {id: 2, title: 'Task 2', status: 0}]
     return await db.query('todoList');
-
   }
 }
--- lib/todo_main.dart
+++ lib/todo_main.dart
@@ -9,84 +9,95 @@ class TodoApp extends StatefulWidget {
 }
 
 class _TodoAppState extends State<TodoApp> {
-
+  // TextEditingController is used to control the text field.
   final TextEditingController _controller = TextEditingController();
-  SqfliteHelper helper = SqfliteHelper();
 
+  //helper is the instance of the SqfliteHelper class.
+  SqfliteHelper helper = SqfliteHelper();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Todo App'),
-      ),
-      body: FutureBuilder(
-        future: helper.getTasks(),
-        builder: (context, snapshot) {
-          return ListView.builder(
-              itemCount:  snapshot.data?.length ?? 0,
-              itemBuilder: (context, index){
-            return ListTile(
-              leading: Checkbox(
-                value: snapshot.data?[index]['status'] == 1 ? true : false,
-                onChanged:(value) async{
-                  await helper.updateTask(snapshot.data?[index]['id'], value!);
-                  setState(() {
-
+        appBar: AppBar(title: const Text('Todo App')),
+        body: FutureBuilder(
+            //helper.getTasks() returns a Future<List<Map<String, dynamic>>>.
+            future: helper.getTasks(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                //snapshot.data is the list of tasks that we get from the database.
+                return ListView.builder(
+                    itemCount: snapshot.data?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                          leading: Checkbox(
+                              //We use 1 for true and 0 for false.
+                              value: snapshot.data?[index]['status'] == 1
+                                  ? true
+                                  : false,
+                              onChanged: (value) async {
+                                //updateTask method takes the id of the task and the status to be updated.
+                                await helper.updateTask(
+                                    snapshot.data?[index]['id'], value!);
+                                //setState is used to update the UI.
+                                setState(() {});
+                              }),
+                          title: Text(snapshot.data?[index]['title']),
+                          trailing: IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () async {
+                                //deleteTask method takes the id of the task to be deleted.
+                                await helper
+                                    .deleteTask(snapshot.data?[index]['id']);
+                                //setState is used to update the UI.
+                                setState(() {});
+                              }));
+                    });
+              }
+              return const Center(child: CircularProgressIndicator());
+            }),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              //showDialog is used to show a dialog box, it takes a context and a builder.
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    //AlertDialog is a dialog box with a title, content, and actions.
+                    return AlertDialog(
+                        title: const Text('Add Task'),
+                        content: TextField(
+                            //autofocus is used to focus the text field when the dialog box is shown.
+                            autofocus: true,
+                            //controller is used to control the text field.
+                            controller: _controller,
+                            decoration: const InputDecoration(
+                              hintText: 'Enter title',
+                            )),
+                        actions: [
+                          TextButton(
+                              onPressed: () {
+                                //Navigator.pop is used to close the dialog box.
+                                Navigator.pop(context);
+                              },
+                              child: const Text('Cancel')),
+                          TextButton(
+                              onPressed: () async {
+                                //insertTask method takes the title of the task and the status.
+                                await helper.insertTask(
+                                    _controller.text, false);
+                                //clear is used to clear the text field.
+                                _controller.clear();
+                                //here we are checking if the widget is mounted before popping the dialog box.
+                                //because we are using context in async functions.
+                                if (context.mounted) {
+                                  Navigator.pop(context);
+                                }
+                                //setState is used to update the UI.
+                                setState(() {});
+                              },
+                              child: const Text('Add'))
+                        ]);
                   });
-                }
-              ),
-              title: Text(snapshot.data?[index]['title']),
-              trailing: IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () async{
-                  await helper.deleteTask(snapshot.data?[index]['id']);
-                  setState(() {
-
-                  });
-                },
-              ),
-            );
-          });
-        }
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  title: const Text('Add Task'),
-                  content: TextField(
-                    controller: _controller,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter title',
-                    ),
-                  ),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text('Cancel'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        helper.insertTask(_controller.text, false);
-                        _controller.clear();
-                        Navigator.pop(context);
-                        setState(() {
-
-                        });
-                      },
-                      child: const Text('Add'),
-                    ),
-                  ],
-                );
-              });
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
+            },
+            child: const Icon(Icons.add)));
   }
 }
